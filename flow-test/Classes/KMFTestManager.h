f9dd5c95b45acb096bd73f925261d26d7e7121be
Nithin <nithin.linkin@gmail.com>
2017-10-12 15:09:08 +0530

    [flow] Add the correct test-case

--- flow-test/Classes/KMFTestManager.h
+++ flow-test/Classes/KMFTestManager.h
@@ -8,7 +8,7 @@
 #import <XCTest/XCTest.h>
 #import "KMFMethodSpec.h"
 
-@interface KMFTestManager : XCTest
+@interface KMFTestManager : XCTestCase
 /// Called before every test is executed.
 - (void)setUp;
 
