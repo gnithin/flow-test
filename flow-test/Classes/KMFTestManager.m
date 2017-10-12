111639ac3e8304984fcb92dc6b48fb078a6903e3
Nithin <nithin.linkin@gmail.com>
2017-10-13 00:53:18 +0530

    [flow] Add a basic specs comparator

--- flow-test/Classes/KMFTestManager.m
+++ flow-test/Classes/KMFTestManager.m
@@ -7,6 +7,8 @@
 
 #import "KMFTestManager.h"
 #import "KMFAspectHandler.h"
+#import "KMFMethodSpec+Internal.h"
+#define localizeString(key) [NSBundle.mainBundle localizedStringForKey:(key) value:@"" table:nil]
 
 @interface KMFTestManager()
 @property (nonatomic) NSArray<KMFMethodSpec *> *specsList;
@@ -57,6 +59,14 @@
     }
     
     [self.aspectHandler removeAllPointCuts];
+    
+    // Perform flow-checking here
+    NSError *flowErr;
+    [self performFlowCheckingWithErr:&flowErr];
+    if(flowErr != nil){
+        // Something went wrong
+        XCTFail("Failed flow-test - %@, with error - %@", self.name, [flowErr localizedDescription]);
+    }
 }
 
 /// This is a method that'll be implemented by the sub-class.
@@ -73,6 +83,55 @@
             [self.calledSpecsList addObject:spec];
         }
     }
+}
+
+- (void)performFlowCheckingWithErr:(NSError **)flowErr{
+    NSString *flowErrStr = nil;
+    if(self.specsList == nil){
+        flowErrStr = @"Specs list cannot be empty!";
+    }else if(self.calledSpecsList == nil){
+        flowErrStr = @"It seems none of the methods in the specs list have been called at all!";
+    }else if([self.calledSpecsList count] > [self.specsList count]){
+        flowErrStr = @"The number of items called are more than what have been listed. Maybe some calls were duplicated!";
+    }else if([self.calledSpecsList count] < [self.specsList count]){
+        flowErrStr = @"The number of items called is less than what have been listed. Maybe some calls were duplicated!";
+    }
+    
+    if(flowErrStr == nil){
+        BOOL specsAreEqual = YES;
+        
+        // Perform the actual mapping comparision here.
+        NSUInteger i;
+        for(i = 0; i<[self.calledSpecsList count]; i++){
+            KMFMethodSpec *expectedSpec = [self.specsList objectAtIndex:i];
+            KMFMethodSpec *actualSpec = [self.calledSpecsList objectAtIndex:i];
+            if(NO == [self areTwoSpecsEqual:expectedSpec :actualSpec]){
+                specsAreEqual = NO;
+                break;
+            }
+        }
+        
+        if(specsAreEqual){
+            return;
+        }
+        flowErrStr = [NSString stringWithFormat:@"Specs are not equal starting from index - %lu", (unsigned long)i];;
+    }
+    
+    // TODO: Add both the lists in a readable way here -
+    (*flowErr) = [NSError errorWithDomain:@"KMFTestManager"
+                               code:-2000
+                           userInfo:@{
+                                      NSLocalizedDescriptionKey: localizeString(flowErrStr),
+                                      }];
+}
+
+- (BOOL)areTwoSpecsEqual:(KMFMethodSpec *)firstSpec :(KMFMethodSpec *)secondSpec{
+    if(firstSpec == nil || secondSpec == nil){
+        return NO;
+    }
+    
+    return ([[firstSpec className] isEqualToString:[secondSpec className]] &&
+            [[firstSpec methodSig] isEqualToString:[secondSpec methodSig]]);
 }
 
 @end
